b18ef2bb3a42ef9e42be2d5656f54fe334df0f7e
mohitraj061 <mohit.raj061@gmail.com>
2025-05-17 01:49:57 +0530

    fixed bugs

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -23,6 +23,6 @@ router.route("/register").post(
 router.route("/login").post(loginUser);
 
 // secure routes
-router.route("logout").post(verifyJWT, logoutUser);
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
